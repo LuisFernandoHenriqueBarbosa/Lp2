4de0ee145b2b276cc3a10482530d12c454a6887b
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-08-13 08:59:00 -0300

    updt0857

--- Java/ProjetoCRUD1/src/data/CRUD.java
+++ Java/ProjetoCRUD1/src/data/CRUD.java
@@ -1,5 +1,17 @@
 package data;
 
-public interface CRUD {
+import java.util.ArrayList;
 
+import model.StatusModel;
+
+public interface CRUD {
+    public boolean incluir(StatusModel obj) throws Exception;
+
+    public boolean excluir(int id) throws Exception;
+
+    public boolean atualizar(StatusModel obj) throws Exception;
+
+    public ArrayList<StatusModel> pesquisar(String pesquisa) throws Exception;
+
+    public StatusModel pesquisar(int id) throws Exception;
 }
--- Java/ProjetoCRUD1/src/data/StatusData.java
+++ Java/ProjetoCRUD1/src/data/StatusData.java
@@ -1,5 +1,41 @@
 package data;
 
-public class StatusData {
+import java.util.ArrayList;
+
+import model.StatusModel;
+
+public class StatusData implements CRUD {
+
+    @Override
+    public boolean incluir(StatusModel obj) throws Exception {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean excluir(int id) throws Exception {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean atualizar(StatusModel obj) throws Exception {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public ArrayList<StatusModel> pesquisar(String pesquisa) throws Exception {
+        // TODO Auto-generated method stub
+        ArrayList<StatusModel> dados = new ArrayList<>();
+        return dados;
+    }
+
+    @Override
+    public StatusModel pesquisar(int id) throws Exception {
+        // TODO Auto-generated method stub
+        StatusModel obj = new StatusModel();
+        return obj;
+    }
 
 }
