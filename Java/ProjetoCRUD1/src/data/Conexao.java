e537d1e6441dd30271e7a03f40c796e95bfc491d
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-08-20 08:45:58 -0300

    up

--- Java/ProjetoCRUD1/src/data/Conexao.java
+++ Java/ProjetoCRUD1/src/data/Conexao.java
@@ -1,21 +1,18 @@
 package data;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-
 public class Conexao {
     private Connection conexao;
-
-    public Connection getConexao() {
+    public Connection getConexao(){
         return conexao;
     }
-
-    public Conexao() throws Exception {
-        String url = "jdbc:sqlserver://localhost:1433;databaseName=bdJava;trustServerCertificate=true;encript=false";
-        String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-        String usuario = "aluno";
-        String senha = "dbo";
+    public Conexao() throws Exception{
+        String url="jdbc:sqlserver://LAB02-MAQ20:1433;databaseName=bdJava;trustServerCertificate=true;encript=false";
+        String driver="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        String usuario="aluno";
+        String senha="dbo";
         Class.forName(driver);
-        conexao = DriverManager.getConnection(url, usuario, senha);
+        conexao = DriverManager.getConnection
+        (url, usuario, senha);
     }
 }
